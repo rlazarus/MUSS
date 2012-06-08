99bf20136791c5c4c0d1f6bdc273e7c85571cf97
Reuven Lazarus <rlazarus@alumni.reed.edu>
2012-06-08 16:47:43 -0700

    Reinvent less wheel: delegate to Word where we can

--- muss/commands.py
+++ muss/commands.py
@@ -174,30 +174,26 @@ class Quit(Command):
         muss.server.factory.allProtocols[player.name].transport.loseConnection()
 
 
-class PlayerName(Token):
+class PlayerName(Word):
     """
     Token to match (case-insensitively) a full player name, regardless of whether that player is nearby.
     """
     _allowed_chars = alphas  # This is temporary; when there are rules for legal player names, we'll draw directly from there.
 
     def __init__(self):
-        super(PlayerName, self).__init__()
+        super(PlayerName, self).__init__(alphas)
     
     def parseImpl(self, instring, loc, doActions=True):
-        string_len = len(instring)
-        match_end = loc
-        while match_end < string_len and instring[match_end] in self._allowed_chars:
-            match_end += 1
-        match = instring[loc:match_end]
+        loc, match = super(PlayerName, self).parseImpl(instring, loc, doActions)
         if player_name_taken(match):
-            return match_end, match
+            return loc, match
         else:
             # pyparsing boilerplate: report failure
             exc = self.myException
             exc.loc = loc
             exc.pstr = instring
             raise exc
-        
+
 
 class Poke(Command):
     name = "poke"
