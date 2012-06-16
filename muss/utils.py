0f5cdb0a9412b52f895dfa78b6117990b3ff9740
Fizz <relsqui@chiliahedron.com>
2012-06-16 11:38:26 -0700

    Added AmbiguityError.

--- muss/utils.py
+++ muss/utils.py
@@ -1,3 +1,17 @@
+from pyparsing import ParseException
+
+class AmbiguityError(ParseException):
+    def __init__(self, pstr, loc=0, msg=None, elem=None, token_name="one", tokens=[]):
+        super(AmbiguityError, self).__init__(pstr, loc, msg, elem)
+        self.token_name = token_name
+        self.tokens = tokens
+
+    def verbose(self):
+        verbose = "Which {} did you mean?".format(self.token_name)
+        if self.tokens:
+            verbose += " ({})".format(", ".join(tokens))
+        return verbose
+
 def find_by_name(name, objects, attribute="names", case_sensitive=False):
     perfect_matches = []
     partial_matches = []
