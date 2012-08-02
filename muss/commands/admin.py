be1bd2ca77add1cd21701d3bd9da569c89e9fdc9
Reuven Lazarus <rlazarus@alumni.reed.edu>
2012-08-01 23:03:45 -0700

    Add a Python REPL. Do not use without reading this commit message in full.
    So far, since there's no support for untrusted user code, the only REPL that makes sense is a trusted REPL. That is, interactive access to *the very Python process* that is the game server itself. This is now available via "sudo python."
    
    If you haven't just soiled your pants in terror, you haven't understood properly.
    
    Direct interactive access means that it's practical to very carefully administer a live game: you can move objects around, edit attributes freely, adjust permissions, and so on, all with SYSTEM access, by means of retrieving the objects from the database and futzing with them. It also means it's practical to debug the server software, which is going to be lovely while we're in development.
    
    It also means that "del muss.db._objects" would run without complaint.
    
    So would "os.system('rm -rf $HOME')".
    
    Treat this function with extreme respect. Even if you never use it, its mere existence is a very real privilege-escalation threat if this project ever gets big enough to attract the people who are into that sort of thing.

--- muss/commands/admin.py
+++ muss/commands/admin.py
@@ -1,7 +1,67 @@
+from code import InteractiveConsole
 from pyparsing import SkipTo, StringEnd
+import sys
+from StringIO import StringIO
 
-from muss.locks import authority_of, SYSTEM
+from muss.handler import Mode, PromptMode
+from muss.locks import LockFailedError, authority, authority_of, SYSTEM
 from muss.parser import Command
+from muss.utils import UserError
+
+
+class Python(Command):
+    name = "python"
+    help_text = "Enter an interactive Python session."
+
+    def execute(self, player, args):
+        if authority() is not SYSTEM:
+            # When user code is implemented, this will create an untrusted REPL under the player's authority.
+            raise UserError("Not yet implemented: for now, sudo is required.")
+
+        player.send("***********")
+        player.send("* WARNING *")
+        player.send("***********")
+        player.send("")
+        player.send("You are working interactively with the actual Python process running the game. Careless actions here can really permanently foul up important things.")
+        player.send("")
+
+        def check_password(line):
+            if player.hash(line) == player.password:
+                with authority_of(SYSTEM):
+                    player.enter_mode(PythonMode(player))
+            else:
+                player.send("Incorrect.")
+
+        player.enter_mode(PromptMode(player, "To proceed, enter your password:", check_password))
+
+
+class PythonMode(Mode):
+    blank_line = False
+
+    def __init__(self, player):
+        if authority() is not SYSTEM:
+            raise LockFailedError("PythonMode requires system authority.")
+
+        self.console = InteractiveConsole()
+        player.send(">>>")
+
+    def handle(self, player, line):
+        if line == ".":
+            player.send("Exiting python.")
+            player.exit_mode()
+            return
+
+        with authority_of(SYSTEM):
+            try:
+                sys.stdout = sys.stderr = StringIO()
+                if self.console.push(line):
+                    player.send("...")
+                else:
+                    player.send(sys.stdout.getvalue())
+                    player.send(">>>")
+            finally:
+                sys.stdout = sys.__stdout__
+                sys.stderr = sys.__stderr__
 
 
 class Sudo(Command):
