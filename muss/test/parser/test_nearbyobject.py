b71c5b111a3805b378bcaa2606ec9e6f63329683
Fizz <relsqui@chiliahedron.com>
2013-06-15 21:08:39 -0700

    Split nearbyobject and reachableobject tests to their own files.

---
+++ muss/test/parser/test_nearbyobject.py
@@ -0,0 +1,130 @@
+from muss import db, parser
+from muss.test import common_tools
+
+
+class NearbyObjectTestCase(common_tools.MUSSTestCase):
+    def setUp(self):
+        super(NearbyObjectTestCase, self).setUp()
+        self.setup_objects()
+        tricky_names = ["me you", "cup of mead", "here there",
+                        "heretical thoughts"]
+        # These are for confounding the me/here keywords.
+        for name in tricky_names:
+            self.objects[name] = common_tools.sudo(lambda:db.Object(name,
+                                                                    self.lobby))
+            db.store(self.objects[name])
+
+    def assert_parse(self, token, string, result):
+        parse_result = token.parseString(string, parseAll=True)
+        self.assertEqual(parse_result[0], result)
+
+    def assert_error_message(self, desired_exception, desired_message,
+                             function_call, *args, **kwargs):
+        """
+        Wrapper for assertRaises which verifies both the exception type and the
+        error message--e.verbose() for any exception extending MatchError, or
+        str(e) for any other exception.
+        """
+        exception = self.assertRaises(desired_exception, function_call,
+                                      *args, **kwargs)
+        if isinstance(exception, parser.MatchError):
+            self.assertEqual(exception.verbose(), desired_message)
+        else:
+            self.assertEqual(str(exception), desired_message)
+
+    def test_nearbyobject_room(self):
+        self.assert_parse(parser.NearbyObject(self.player), "lobby", self.lobby)
+
+    def test_nearbyobject_my_success(self):
+        near_player = parser.NearbyObject(self.player)
+        for phrase in ["my apple", "my app"]:
+            self.assert_parse(near_player, phrase, self.objects["apple"])
+        self.assert_parse(near_player, "my horse",
+                          self.objects["horse figurine"])
+        self.assert_parse(near_player, "my cat", self.objects["inv_cat"])
+
+    def test_nearbyobject_my_ambiguous(self):
+        self.assertRaises(parser.AmbiguityError,
+                          parser.NearbyObject(self.player).parseString,
+                          "my ap", parseAll=True)
+
+    def test_nearbyobject_my_notfound(self):
+        for item in ["ant", "frog", "asdf"]:
+            pattern = parser.NearbyObject(self.player)
+            self.assert_error_message(parser.NotFoundError,
+                                      "I don't know of an object in your "
+                                      "inventory called \"{}\"".format(item),
+                                      pattern.parseString,
+                                      "my " + item, parseAll=True)
+
+    def test_nearbyobject_nopriority_success(self):
+        for item in ["ant", "frog", "apple", "ape plushie"]:
+            self.assert_parse(parser.NearbyObject(self.player), item,
+                              self.objects[item])
+
+    def test_nearbyobject_nopriority_ambiguous(self):
+        for item in ["a", "cat", "h"]:
+            self.assertRaises(parser.AmbiguityError,
+                              parser.NearbyObject(self.player).parseString,
+                              item, parseAll=True)
+
+    def test_nearbyobject_nopriority_notfound(self):
+        self.assert_error_message(parser.NotFoundError,
+                                  "I don't know of a nearby object called "
+                                  "\"asdf\"",
+                                  parser.NearbyObject(self.player).parseString,
+                                  "asdf", parseAll=True)
+
+    def test_nearbyobject_priority_success(self):
+        items = [("an", "ant"), ("horse", "horse"), ("ho", "horse"),
+                 ("cher", "cherry"), ("cheese", "cheese")]
+        pattern = parser.NearbyObject(self.player, priority="room")
+
+        for name, item in items:
+            self.assert_parse(pattern, item, self.objects[item])
+        self.assert_parse(pattern, "cat", self.objects["room_cat"])
+
+        pattern = parser.NearbyObject(self.player, priority="inventory")
+        self.assert_parse(pattern, "cat", self.objects["inv_cat"])
+
+    def test_nearbyobject_priority_ambiguous(self):
+        pattern = parser.NearbyObject(self.player, priority="room")
+        self.assertRaises(parser.AmbiguityError,
+                          pattern.parseString, "f", parseAll=True)
+        self.assertRaises(parser.AmbiguityError, pattern.parseString,
+                          "ch", parseAll=True)
+        e = self.assertRaises(parser.AmbiguityError, pattern.parseString,
+                              "a", parseAll=True)
+        a_names = ["abacus", "ant"]
+        a_matches = [(i, self.objects[i]) for i in a_names]
+        self.assertEqual(sorted(e.matches), sorted(a_matches))
+
+    def test_nearbyobject_priority_notfound(self):
+        pattern = parser.NearbyObject(self.player, priority="inventory")
+        self.assert_error_message(parser.NotFoundError,
+                                  "I don't know of a nearby object called "
+                                  "\"asdf\"",
+                                  pattern.parseString, "asdf", parseAll=True)
+
+    def test_nearbyobject_priority_badpriority(self):
+        self.assertRaises(ValueError,
+                          parser.NearbyObject, self.player, priority="foo")
+
+    def test_nearbyobject_player(self):
+        pattern = parser.NearbyObject(self.player)
+        self.assert_parse(pattern, "PlayersNeighbor", self.neighbor)
+
+    def test_nearbyobject_me(self):
+        pattern = parser.NearbyObject(self.player)
+        self.assert_parse(pattern, "me", self.player)
+        me = common_tools.sudo(lambda:db.Object("me", self.lobby))
+        db.store(me)
+        self.assert_parse(pattern, "me", me)
+
+    def test_nearbyobject_here(self):
+        pattern = parser.NearbyObject(self.player)
+        self.assert_parse(pattern, "here", self.lobby)
+        here = common_tools.sudo(lambda:db.Object("here", self.lobby))
+        db.store(here)
+        self.assert_parse(pattern, "here", here)
+        # Just because this works doesn't mean you should ever do it.
---
+++ muss/test/parser/test_reachableobject.py
@@ -0,0 +1,126 @@
+import pyparsing as pyp
+
+from muss import db, parser
+from muss.test import common_tools
+
+
+class ReachableObjectTestCase(common_tools.MUSSTestCase):
+    def setUp(self):
+        super(ReachableObjectTestCase, self).setUp()
+        self.setup_objects()
+        tricky_names = ["me you", "cup of mead", "here there",
+                        "heretical thoughts"]
+        # These are for confounding the me/here keywords.
+        for name in tricky_names:
+            self.objects[name] = common_tools.sudo(lambda:db.Object(name,
+                                                                    self.lobby))
+            db.store(self.objects[name])
+
+    def assert_parse(self, token, string, result):
+        parse_result = token.parseString(string, parseAll=True)
+        self.assertEqual(parse_result[0], result)
+
+    def assert_error_message(self, desired_exception, desired_message,
+                             function_call, *args, **kwargs):
+        """
+        Wrapper for assertRaises which verifies both the exception type and the
+        error message--e.verbose() for any exception extending MatchError, or
+        str(e) for any other exception.
+        """
+        exception = self.assertRaises(desired_exception, function_call,
+                                      *args, **kwargs)
+        if isinstance(exception, parser.MatchError):
+            self.assertEqual(exception.verbose(), desired_message)
+        else:
+            self.assertEqual(str(exception), desired_message)
+
+    def test_reachableobject_nearby_success(self):
+        pattern = parser.ReachableObject(self.player)
+        for item in ["apple", "frog"]:
+            self.assert_parse(pattern, item, self.objects[item])
+        self.assert_parse(pattern, "my ape", self.objects["ape plushie"])
+
+    def test_reachableobject_nearby_failure(self):
+        pattern = parser.ReachableObject(self.player)
+        self.assert_error_message(parser.NotFoundError,
+                                  "I don't know of a reachable object called "
+                                  "\"asdf\"",
+                                  pattern.parseString, "asdf", parseAll=True)
+
+    def test_reachableobject_simple_success(self):
+        pattern = parser.ReachableObject(self.player)
+        self.assert_parse(pattern, "cat on Player", self.objects["inv_cat"])
+        self.assert_parse(pattern, "apple in Player", self.objects["apple"])
+        self.assert_parse(pattern, "cat in room", self.objects["room_cat"])
+        self.assert_parse(pattern, "apple in playersneighbor",
+                          self.objects["neighbor_apple"])
+        self.assert_parse(pattern, "PlayersNeighbor's apple",
+                          self.objects["neighbor_apple"])
+
+    def test_reachableobject_preposition_failure(self):
+        pattern = parser.ReachableObject(self.player)
+        self.assert_error_message(parser.NotFoundError,
+                                  "I don't know of a reachable object called "
+                                  "\"foo between bar\"",
+                                  pattern.parseString,
+                                  "foo between bar", parseAll=True)
+
+    def test_reachableobject_preposition_player_failure(self):
+        pattern = parser.ReachableObject(self.player)
+        self.assert_error_message(parser.NotFoundError,
+                                  "I don't know of an object in "
+                                  "PlayersNeighbor's inventory called \"asdf\"",
+                                  pattern.parseString,
+                                  "asdf in playersneighbor", parseAll=True)
+
+    def test_reachable_object_room_failure(self):
+        pattern = parser.ReachableObject(self.player)
+        self.assert_error_message(parser.NotFoundError,
+                                  "I don't know of an object in lobby called "
+                                  "\"cherry\"",
+                                  pattern.parseString,
+                                  "cherry in room", parseAll=True)
+
+    def test_reachableobject_owner_failure(self):
+        pattern = parser.ReachableObject(self.player)
+        self.assert_error_message(parser.NotFoundError,
+                                  "I don't know of an object in "
+                                  "PlayersNeighbor's inventory called \"frog\"",
+                                  pattern.parseString,
+                                  "PlayersNeighbor's frog", parseAll=True)
+
+    def test_reachableobject_combining_success(self):
+        grammar = (parser.ReachableObject(self.player)("first") +
+                   pyp.CaselessKeyword("and") +
+                   parser.ReachableObject(self.player)("second"))
+        parse_result = grammar.parseString("apple in player and hat on frog",
+                                           parseAll=True)
+        self.assertEqual(parse_result["first"], self.objects["apple"])
+        self.assertEqual(parse_result["second"], self.objects["hat"])
+        parse_result = grammar.parseString("hat on frog and Fodor's",
+                                           parseAll=True)
+        self.assertEqual(parse_result["first"], self.objects["hat"])
+        self.assertEqual(parse_result["second"], self.objects["Fodor's Guide"])
+
+    def test_reachableobject_combining_failure(self):
+        grammar = (parser.ReachableObject(self.player)("first") +
+                   pyp.CaselessKeyword("and") +
+                   parser.ReachableObject(self.player)("second"))
+        self.assert_error_message(parser.NotFoundError,
+                                  "I don't know of an object in frog called "
+                                  "\"apple and hat\"",
+                                  grammar.parseString,
+                                  "apple and hat on frog", parseAll=True)
+
+    def test_reachableobject_combining_owner(self):
+        grammar = (parser.ReachableObject(self.player)("first") +
+                   pyp.CaselessKeyword("and") +
+                   parser.ReachableObject(self.player)("second"))
+        parse_result = grammar.parseString("my apple and PlayersN's apple",
+                                           parseAll=True)
+        self.assertEqual(parse_result["first"], self.objects["apple"])
+        self.assertEqual(parse_result["second"], self.objects["neighbor_apple"])
+        parse_result = grammar.parseString("PlayersN's apple and frog in room",
+                                           parseAll=True)
+        self.assertEqual(parse_result["first"], self.objects["neighbor_apple"])
+        self.assertEqual(parse_result["second"], self.objects["frog"])
