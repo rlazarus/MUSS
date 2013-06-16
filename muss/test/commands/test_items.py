a1af593a12b546179b484c4f65799b476871a30d
Fizz <relsqui@chiliahedron.com>
2013-06-15 20:57:32 -0700

    Split up more command tests.

--- muss/test/commands/test_items.py
+++ muss/test/commands/test_items.py
@@ -128,32 +128,3 @@ class ItemTestCase(common_tools.MUSSTestCase):
             self.objects["monocle"].equipped = True
             self.objects["monocle"].location = self.lobby
         self.assertEqual(self.objects["monocle"].equipped, False)
-
-    def test_give_fail(self):
-        self.assertEqual(self.objects["monocle"].location, self.player)
-        self.assert_response("give monocle to playersneighbor",
-                             "You can't put that in PlayersNeighbor.")
-        self.assertEqual(self.objects["monocle"].location, self.player)
-
-    def test_give_succeed(self):
-        self.neighbor.locks.insert = locks.Pass()
-        self.assertEqual(self.objects["monocle"].location, self.player)
-        self.assert_response("give monocle to playersneighbor",
-                             "You give monocle to PlayersNeighbor.")
-        self.assertEqual(self.objects["monocle"].location, self.neighbor)
-
-    def test_put_fail_drop(self):
-        self.objects["cherry"].locks.drop = locks.Fail()
-        self.assert_response("put cherry in bucket", "You cannot drop cherry.")
-
-    def test_put_fail_insert(self):
-        self.objects["Bucket"].locks.insert = locks.Fail()
-        self.assert_response("put cherry in bucket",
-                             "You can't put that in Bucket.")
-
-    def test_put_succeed(self):
-        self.assert_response("give bucket cherry", "You put cherry in Bucket.")
-        self.assertEqual(self.objects["cherry"].location,
-                         self.objects["Bucket"])
-        self.assert_response("put moose in bucket", "You put moose in Bucket.")
-        self.assertEqual(self.objects["moose"].location, self.objects["Bucket"])
